c4e87705b6441dab553ac4b6da532e993c2d7b40
ranburu1 <gyadas3347@gmail.com>
2021-09-18 19:53:35 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true 
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true,
+  format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+  validates :first_name, presence: true,
+  format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+  validates :last_name_kana, presence: true,
+  format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana, presence: true,
+  format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
   validates :birthday, presence: true
+  validates :password,
+  format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/, message: "is invalid. Include both letters and numbers
+    ."}
+  
 end
