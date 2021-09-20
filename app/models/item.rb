c13c191e45c7370e0206c4d7b11b6f8a52517d4a
ranburu1 <gyadas3347@gmail.com>
2021-09-20 18:13:45 +0900

    validates 設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
  extend ActiveHash::Associations::ActiveRecordExtensions
 
+ validates :title,            presence: true
+ validates :explanatory_text, presence: true
+ validates :category_id,      presence: true, numericality: { other_than: 1 }
+ validates :status_id,        presence: true, numericality: { other_than: 1 }
+ validates :price,            presence: true
+ validates :shipping_fee_id,  presence: true, numericality: { other_than: 1 }
+ validates :area_id,          presence: true, numericality: { other_than: 1 }
+ validates :delivery_days_id, presence: true, numericality: { other_than: 1 }
+ validates :image,            presence: true
+
  has_one :order
  belongs_to :user
  belongs_to :category
@@ -10,3 +20,14 @@ class Item < ApplicationRecord
  belongs_to :area
  has_one_attached :image
 end
+| Column             | Type       | Options                        |
+| ------------------ | ---------- | ------------------------------ |
+| title              | string     | null: false                    |
+| explanatory_text   | text       | null: false                    |
+| category_id        | integer    | null: false                    |
+| status_id          | integer    | null: false                    |
+| price              | integer    | null: false                    |
+| shipping_fee_id    | integer    | null: false                    |
+| area_id            | integer    | null: false                    |
+| delivery_days_id   | integer    | null: false                    |
+| user               | references | null: false, foreign_key: true |
