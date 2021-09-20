a7a0aed5f9d77bd18b1fa159fc67143ec90212fc
ranburu1 <gyadas3347@gmail.com>
2021-09-20 20:21:08 +0900

    ストロングパラメーター記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,9 @@ class ItemsController < ApplicationController
   def create
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:title, :explanatory_text, :category_id, :status_id, :price, :shipping_fee_id, :area_id, :delivery_days_id).merge(user_id: current_user.id)
+  end
 end
+|
