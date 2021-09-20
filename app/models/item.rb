e741d7cc3d25ef3f31671d1e5002a77956122731
ranburu1 <gyadas3347@gmail.com>
2021-09-20 17:55:26 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+ extend ActiveHash::Associations::ActiveRecordExtensions
+
  has_one :order
  belongs_to :user
+ belongs_to :category
+ belongs_to :status
+ belongs_to :shipping_fee
+ belongs_to :delivery_days
+ belongs_to :area
  has_one_attached :image
 end
--- db/migrate/20210920014057_create_items.rb
+++ db/migrate/20210920014057_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :title
-      t.text :explanatory_text
-      t.integer :category_id
-      t.integer :status_id
-      t.integer :price
-      t.integer :shipping_fee_id
-      t.integer :area_id
-      t.integer :delivery_days_id
+      t.string     :title,            null: false
+      t.text       :explanatory_text, null: false 
+      t.integer    :category_id,      null: false
+      t.integer    :status_id,        null: false
+      t.integer    :price,            null: false
+      t.integer    :shipping_fee_id,  null: false
+      t.integer    :area_id,          null: false
+      t.integer    :delivery_days_id, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
