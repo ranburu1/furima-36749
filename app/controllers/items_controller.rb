95e3444660186adb0499bc5852715763c51aa4b6
ranburu1 <gyadas3347@gmail.com>
2021-09-23 15:34:55 +0900

    削除後トップページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,11 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
-
-
-
   private
 
   def item_params
