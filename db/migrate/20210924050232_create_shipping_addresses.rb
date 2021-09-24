39179f15a4ca7788fd396ffc9f8dce9af56525f5
ranburu1 <gyadas3347@gmail.com>
2021-09-24 14:40:38 +0900

    マイグレーションファイル生成

--- db/migrate/20210924050232_create_shipping_addresses.rb
+++ db/migrate/20210924050232_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string     :postcode,     null: false
+      t.integer    :area_id,      null: false
+      t.string     :city,         null: false
+      t.string     :block,        null: false
+      t.string     :building
+      t.integer    :phone_number, null: false
+      t.references :order,        null: false, foreign_key: true
       t.timestamps
     end
   end
