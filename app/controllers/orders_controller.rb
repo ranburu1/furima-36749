92ab45ca136205e13371b1043d1b5f8ed68df6a9
ranburu1 <gyadas3347@gmail.com>
2021-09-24 21:37:13 +0900

    ２つのテーブルへの保存記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    @order_shipping_address= OrderShippingAddress.new
+    @order_shipping_address = OrderShippingAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_shipping_address.valid?
+    
+     @order_shipping_address.save
+       redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,20 +1,18 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :area_id, :city, :block, :building, :phone_number, :order_id
-
+  attr_accessor :user_id, :item_id, :postcode, :area_id, :city, :block, :building, :phone_number
+  validates :area_id, numericality: { other_than: 1 }
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :area_id, numericality: { other_than: 1 }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :order_id
   end
 
   def save
-    @order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postcode: postcode, area_id: area_id, city: city, block: block, building: building, phone_number: phone_number, order_id: @order_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postcode: postcode, area_id: area_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
