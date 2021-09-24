5e3d178219dee1f2bee66be1e592ffecae28b1dc
ranburu1 <gyadas3347@gmail.com>
2021-09-24 13:53:57 +0900

    テーブルに保存する処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,12 @@ class OrderAddress
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-    validates :postode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :order_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, area_id: area_id, city: city, block: block, building: building, phone_number: phone_number, :order_id order_id)
   end
 end
