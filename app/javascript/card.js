21e43e8e1463e5496cf12dfefeac0f318c522abe
ranburu1 <gyadas3347@gmail.com>
2021-09-27 11:12:25 +0900

    トークンをサーバーサイドへ送信する記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,12 +13,21 @@ const pay = () => {
       exp_month: formData.get("order_shipping_address[exp_month]"),
       exp_year: `20${formData.get("order_shipping_address[exp_year]")}`,
     };
-    
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
